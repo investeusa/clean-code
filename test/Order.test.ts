657b70dfcabc7c23a258064bf1b977d949893dd7
Investe USA <investeusa@gmail.com>
2022-08-03 23:20:11 -0300

    Deve criar um pedido com 3 itens:
    corrigir inveja de dados de ordem item

--- test/Order.test.ts
+++ test/Order.test.ts
@@ -1,3 +1,4 @@
+import Item from "../src/Item";
 import Order from "../src/Order";
 
 test("Deve criar um pedido", function () {
@@ -6,6 +7,11 @@ test("Deve criar um pedido", function () {
     expect(order.getTotal()).toBe(0);
 });
 
-// test("Deve criar um pedido com 3 itens", function () {
-
-// })
+test("Deve criar um pedido com 3 itens", function () {
+    const order = new Order("935.411.347-80");
+    order.addItem(new Item(1, "Instumentos musicais", "Guitarra", 1000), 1)
+    order.addItem(new Item(2, "Instumentos musicais", "Amplificador", 5000), 1)
+    order.addItem(new Item(3, "Instumentos musicais", "Guitarra", 30), 3)
+    const total = order.getTotal();
+    expect(total).toBe(6090)
+})
