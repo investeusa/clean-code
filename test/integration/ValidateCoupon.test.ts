bc5159fbf6358667411306433d5142352a19e631
Investe USA <investeusa@gmail.com>
2022-08-09 22:44:11 -0300

    Update ValidateCoupon.test.ts

--- test/integration/ValidateCoupon.test.ts
+++ test/integration/ValidateCoupon.test.ts
@@ -1,7 +1,7 @@
 import ValidateCoupon from "../../src/application/usecase/validate-coupon/ValidateCoupon";
 import CouponRepositoryMemory from "../../src/infra/repository/memory/CouponRepositoryMemory"
 
-test("Deve validarr um cupom", function () {
+test("Deve validar um cupom", function () {
     const counponRepository = new CouponRepositoryMemory();
     const validateCoupon = new ValidateCoupon(counponRepository);
     const isValid = validateCoupon.execute("VALE20");
